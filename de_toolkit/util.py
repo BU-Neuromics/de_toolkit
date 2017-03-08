fce798dab7e9b614d9d1209576b85558d66f64d3
Adam Labadorf <alabadorf@gmail.com>
2017-03-08 16:55:05 -0500

    fixed common import, added rpy2 and stub decorators

--- de_toolkit/util.py
+++ de_toolkit/util.py
@@ -1,2 +1,20 @@
+from .common import CountMatrix
+
 def load_count_mat_file(count_fn) :
-  pass
+  with open(count_fn) as f :
+    count_obj = CountMatrix(f)
+  return count_obj
+
+def require_rpy2(f) :
+  try :
+    import rpy2
+  except ImportError as e :
+    raise Exception('rpy2 must be installed to use this function')
+
+  return f
+
+class Stub(Exception): pass
+def stub(f) :
+  def stub(*args,**kwargs) :
+    raise Stub('Not yet implemented - {}.{}'.format(f.__module__,f.__name__))
+  return stub

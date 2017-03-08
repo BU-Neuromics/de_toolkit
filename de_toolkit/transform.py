b72739bc9e0fbb94e22fc0b52495abbadbad5aa0
Adam Labadorf <alabadorf@gmail.com>
2017-03-08 16:54:30 -0500

    added cli usage to transform.py

--- de_toolkit/transform.py
+++ de_toolkit/transform.py
@@ -1,3 +1,13 @@
+'''
+Usage:
+  detk-transform vst <counts_fn>
+  detk-transform ruvseq <counts_fn>
+  detk-transform trim <counts_fn>
+  detk-transform shrink <counts_fn>
+'''
+from docopt import docopt
+from .util import stub, load_count_mat_file, require_rpy2
+
 def pmf_transform(x,shrink_factor=0.25,max_p=None,iters=1000) :
 
   x = x.copy()
@@ -26,15 +36,39 @@ def pmf_transform(x,shrink_factor=0.25,max_p=None,iters=1000) :
 
   return x
 
+@stub
 def shrink_outliers(count_mat) :
   pass
 
+@stub
 def trim_outliers(count_mat) :
   pass
 
+@require_rpy2
 def vst(count_mat) :
   pass
 
+@stub
 def ruvseq(count_mat) :
   pass
 
+def main(argv=None) :
+
+  args = docopt(__doc__,argv=argv)
+
+  count_obj = load_count_mat_file(args['<counts_fn>'])
+
+  if '<cov_fn>' in args :
+    count_obj.add_covariates(args['<cov_fn>'])
+
+  if args['vst'] :
+    vst(count_obj)
+  elif args['ruvseq'] :
+    ruvseq(count_obj)
+  elif args['trim'] :
+    trim_outliers(count_obj)
+  elif args['shrink'] :
+    shrink_outliers(count_obj)
+
+if __name__ == '__main__' :
+  main()

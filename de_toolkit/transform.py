2e7037502bb8124275e2f6a955602450eeecb7b4
Adam Labadorf <alabadorf@gmail.com>
2017-03-08 20:03:31 -0500

    first complete vst function calling rpy2 and DESeq2

--- de_toolkit/transform.py
+++ de_toolkit/transform.py
@@ -1,12 +1,19 @@
 '''
 Usage:
-  detk-transform vst <counts_fn>
+  detk-transform vst <counts_fn> <cov_fn>
   detk-transform ruvseq <counts_fn>
   detk-transform trim <counts_fn>
   detk-transform shrink <counts_fn>
 '''
 from docopt import docopt
-from .util import stub, load_count_mat_file, require_rpy2
+from .util import (
+  count_obj_to_DESeq2
+  ,load_count_mat_file
+  ,require_rpy2
+  ,require_deseq2
+  ,rpy2_console_wrapper
+  ,stub
+)
 
 def pmf_transform(x,shrink_factor=0.25,max_p=None,iters=1000) :
 
@@ -37,19 +44,42 @@ def pmf_transform(x,shrink_factor=0.25,max_p=None,iters=1000) :
   return x
 
 @stub
-def shrink_outliers(count_mat) :
+def shrink_outliers(count_obj) :
   pass
 
 @stub
-def trim_outliers(count_mat) :
+def trim_outliers(count_obj) :
   pass
 
-@require_rpy2
-def vst(count_mat) :
-  pass
+@require_deseq2
+def vst(count_obj) :
+  from rpy2 import robjects
+  from rpy2.rinterface import RRuntimeError
+  import rpy2.rlike.container as rlc
+  from rpy2.robjects.packages import importr
+  import warnings
+
+  base = importr('base')
+
+  # TODO: consider wrapping this in a try except
+  deseq = importr('DESeq2')
+  sumexp = importr('SummarizedExperiment')
+
+  dds = count_obj_to_DESeq2(count_obj)
+
+  with rpy2_console_wrapper() as rpy2_f :
+    try :
+      vsd = deseq.varianceStabilizingTransformation(dds)
+      vsd_values = sumexp.assay(vsd)
+    except RRuntimeError as e :
+      print(e)
+      raise
+    print(rpy2_f.getvalue())
+
+  return vsd_values
 
 @stub
-def ruvseq(count_mat) :
+def ruvseq(count_obj) :
   pass
 
 def main(argv=None) :
@@ -58,11 +88,12 @@ def main(argv=None) :
 
   count_obj = load_count_mat_file(args['<counts_fn>'])
 
-  if '<cov_fn>' in args :
-    count_obj.add_covariates(args['<cov_fn>'])
+  if args['<cov_fn>'] is not None :
+    count_obj.add_column_data(args['<cov_fn>'])
 
   if args['vst'] :
-    vst(count_obj)
+    count_obj.transform(vst)
+
   elif args['ruvseq'] :
     ruvseq(count_obj)
   elif args['trim'] :

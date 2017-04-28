e3d231f82b4c729540aa34e5e9528a00c2c142db
Adam Labadorf <alabadorf@gmail.com>
2017-04-28 15:43:08 -0400

    CountMatrix and CountMatrixFile tests

--- tests/common/test_common.py
+++ tests/common/test_common.py
@@ -12,14 +12,73 @@ def test_CountMatrix(
   ) :
 
   from de_toolkit import CountMatrix
+  from de_toolkit.common import SampleMismatchException
 
+  # with no column data
   mat = CountMatrix(
     fake_counts_pandas_dataframe
-    ,index=None
-    ,columns=None
-    ,column_data=fake_column_data_pandas_dataframe
   )
 
-def test_CountMatrixFile() :
+  # with explicit sample and count names
+  mat = CountMatrix(
+    fake_counts_pandas_dataframe
+    ,sample_names=fake_counts_pandas_dataframe.columns
+    ,count_names=fake_counts_pandas_dataframe.index
+  )
+  assert all(mat.sample_names == fake_counts_pandas_dataframe.columns)
+  assert all(mat.count_names == fake_counts_pandas_dataframe.index)
+
+  # with no explicit index or names
+  mat = CountMatrix(
+    fake_counts_pandas_dataframe
+    ,column_data=fake_column_data_pandas_dataframe
+  )
+  assert all(mat.sample_names == fake_column_data_pandas_dataframe.index)
+  assert all(mat.count_names == fake_counts_pandas_dataframe.index)
+
+  # with strict
+  mat = CountMatrix(
+    fake_counts_pandas_dataframe
+    ,column_data=fake_column_data_pandas_dataframe
+    ,strict=True
+  )
+  assert all(mat.sample_names == fake_column_data_pandas_dataframe.index)
+  assert all(mat.count_names == fake_counts_pandas_dataframe.index)
+
+  # with violations of strict
+  # change the order of count columns wrt column data
+  fake_counts_pandas_dataframe.columns = fake_counts_pandas_dataframe.columns[::-1]
+  with pytest.raises(SampleMismatchException) :
+    mat = CountMatrix(
+      fake_counts_pandas_dataframe
+      ,column_data=fake_column_data_pandas_dataframe
+      ,strict=True
+    )
+
+  # add an additional column
+  fake_counts_pandas_dataframe['d'] = 0
+  with pytest.raises(SampleMismatchException) :
+    mat = CountMatrix(
+      fake_counts_pandas_dataframe
+      ,column_data=fake_column_data_pandas_dataframe
+      ,strict=True
+    )
+  
+  # with different sample names and not strict
+  mat = CountMatrix(
+    fake_counts_pandas_dataframe
+    ,column_data=fake_column_data_pandas_dataframe
+    ,strict=False
+  )
+  assert sorted(mat.sample_names) == sorted(fake_column_data_pandas_dataframe.index)
+
+def test_CountMatrixFile(
+  fake_counts_csv
+  ,fake_column_data_csv
+  ) :
   from de_toolkit import CountMatrixFile
-  assert False
+
+  mat = CountMatrixFile(
+    fake_counts_csv
+    ,column_data_f=fake_column_data_csv
+  )

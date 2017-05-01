55ddb29870f0747a0684ffe97d71799a8202fd73
Adam Labadorf <alabadorf@gmail.com>
2017-05-01 14:17:08 -0400

    added bad counts test for Firth

--- tests/de/test_de.py
+++ tests/de/test_de.py
@@ -49,3 +49,44 @@ def test_firth_w_bad_cov(fake_counts_obj) :
 
   with pytest.raises(InvalidDesignException) :
     firth_out = firth_logistic_regression(fake_counts_obj)
+
+
+# for issue #4, "no memory available" in logistf.c is caused by improper
+# handling of an empty sample name intersection between counts and covs,
+# case covered in common/test_common.py
+
+
+# tests for:
+#   - profile likelihood estimation crash (issue #3)
+def test_firth_bad_counts() :
+  import os
+  import pandas
+  from de_toolkit.common import CountMatrix
+  from de_toolkit.de import firth_logistic_regression
+
+  base_dir = os.path.join('tests','de')
+
+  # bad_counts.csv contains real gene data that have been found to give
+  # Firth difficulty, in order:
+  #   1. gene is fine
+  #   2. gene crashes on the profile likelihood method, and so reverts to Wald
+  counts = pandas.read_csv(
+    os.path.join(base_dir,'bad_counts.csv')
+    ,index_col=0
+  )
+  cov = pandas.read_csv(
+    os.path.join(base_dir,'sample_info__batch_PD3.csv')
+    ,index_col=1
+  )
+
+  mat = CountMatrix(
+    counts
+    ,column_data=cov
+    ,design='Condition ~'
+  )
+
+  firth_out = firth_logistic_regression(mat)
+
+  assert firth_out.status[0] == 'PL'
+  assert firth_out.status[1] == 'Wald'
+

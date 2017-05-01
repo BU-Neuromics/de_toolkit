01f84af33e7120a797776144fbd5d2c8036606cd
Adam Labadorf <alabadorf@gmail.com>
2017-05-01 12:03:33 -0400

    added git and git-lfs, tracking exact conda packages

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,1 +1,3 @@
-conda install python=3.5 docopt pandas pytest "rpy2>=2.7.3" bioconductor-deseq2 future
+conda install python=3.5 docopt pandas pytest "rpy2>=2.7.3" bioconductor-deseq2 \
+  future "git>=1.8.2" git-lfs
+conda list > conda_packages.txt

1ec7666657f20b7eb85a8aa06b5066919c0a73cb
Adam Labadorf <alabadorf@gmail.com>
2017-05-12 17:47:07 -0400

    added counts term explicitly to design tests

--- tests/de/test_de.py
+++ tests/de/test_de.py
@@ -7,22 +7,22 @@ def test_de_cli() :
     main(argv=None)
 
 def test_firth_cli(fake_counts_csv,fake_column_data_csv) :
-  main(['firth','category ~ ',fake_counts_csv,fake_column_data_csv])
+  main(['firth','category ~ counts',fake_counts_csv,fake_column_data_csv])
 
 def test_firth_cli_w_cov(fake_counts_csv,fake_column_data_csv) :
-  main(['firth','category ~ cont_cov',fake_counts_csv,fake_column_data_csv])
+  main(['firth','category ~ cont_cov + counts',fake_counts_csv,fake_column_data_csv])
 
 def test_firth(fake_counts_obj) :
   from de_toolkit.de import firth_logistic_regression
 
-  fake_counts_obj.add_design('category ~')
+  fake_counts_obj.add_design('category ~ counts')
 
   firth_out = firth_logistic_regression(fake_counts_obj)
 
 def test_firth_w_cov(fake_counts_obj) :
   from de_toolkit.de import firth_logistic_regression
 
-  fake_counts_obj.add_design('category ~ cont_cov')
+  fake_counts_obj.add_design('category ~ cont_cov + counts')
 
   firth_out = firth_logistic_regression(fake_counts_obj)
 

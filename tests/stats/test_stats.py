a1b8dd78d22438c7f099122df1b1c520fe598035
Kylie Shen <kashen@bu.edu>
2017-06-05 12:47:51 -0400

    Altered test for base function in test_stats.py to accept JSON output

--- tests/stats/test_stats.py
+++ tests/stats/test_stats.py
@@ -3,11 +3,13 @@ import numpy as np
 import os                                                                       
 import pandas                                                                   
 import pytest                                                                   
-import tempfile                                                                 
+import tempfile                       
+import json                                          
 
 from de_toolkit.stats import base                                                                               
 def test_stats_base(fake_counts_obj): 
-	output = base(fake_counts_obj)
+	json_output = base(fake_counts_obj)
+	output=json.loads(json_output)
 	cols = output.get('stats', {}).get('num_cols')
 	rows = output.get('stats', {}).get('num_rows')
 	assert cols==3 and rows==5 

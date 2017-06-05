d5aadf1c6c54db8c9dae672bad262ca855c5030b
Kylie Shen <kashen@bu.edu>
2017-06-05 13:07:38 -0400

    Fixed bug in stats.py colzero function and wrote test to ensure that colzero function gets correct column names

--- tests/stats/test_stats.py
+++ tests/stats/test_stats.py
@@ -14,5 +14,19 @@ def test_stats_base(fake_counts_obj):
 	rows = output.get('stats', {}).get('num_rows')
 	assert cols==3 and rows==5 
 
+from de_toolkit.stats import colzero
 
+def test_stats_colzero_names(fake_counts_obj):
+	json_output = colzero(fake_counts_obj)
+	output=json.loads(json_output)
+	zeros = output.get('stats', {}).get('zeros')
+
+	true_col_names = fake_counts_obj.sample_names.tolist()
+	col_names = []
+	for i in range(0, len(zeros)):
+		col = zeros[i]
+		name = col.get('name')
+		col_names.append(name)
+	
+	assert true_col_names==col_names
 

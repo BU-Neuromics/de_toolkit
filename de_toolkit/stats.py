f17433248fdc07f9fe87cd943b04be17698baefc
Angel Dai <angelD@Angels-MacBook-Pro.local>
2017-06-05 20:36:40 -0400

    fixed formatting issues

--- de_toolkit/stats.py
+++ de_toolkit/stats.py
@@ -54,19 +54,19 @@ def coldist(count_mat) :
         
     output['stats']['dists'] = []
     for s in count_mat.sample_names:
-    #to access the data in each column
-    data = getattr(count_mat.counts,s)
+        #to access the data in each column
+        data = getattr(count_mat.counts,s)
                 
-    #for the upper and lower outliers
-    Q1 = np.percentile(data, 25)
-    Q3 = np.percentile(data, 75)
-    IQR =  np.percentile(data, 75) - np.percentile(data, 25)
+        #for the upper and lower outliers
+        Q1 = np.percentile(data, 25)
+        Q3 = np.percentile(data, 75)
+        IQR =  np.percentile(data, 75) - np.percentile(data, 25)
                 
-    #for the histogram bin edges and count numbers
-    (n, bins, patches) = plt.hist(data, bins=20, label='hst')
+        #for the histogram bin edges and count numbers
+        (n, bins, patches) = plt.hist(data, bins=20, label='hst')
                 
-    #make the dict for each sample
-    output['stats']['dists'].append({'name':s, 'dist':list(n), 'bins':list(bins)[1:],'extrema':{'lower':[i for i in data if i < Q1-1.5*IQR], 'upper':[i for i in data if i > Q3+1.5*IQR]}})
+        #make the dict for each sample
+        output['stats']['dists'].append({'name':s, 'dist':list(n), 'bins':list(bins)[1:],'extrema':{'lower':[i for i in data if i < Q1-1.5*IQR], 'upper':[i for i in data if i > Q3+1.5*IQR]}})
         
     return json.dumps(output, indent = 4, sort_keys = True)
 
@@ -83,18 +83,18 @@ def rowdist(count_mat) :
         #to access the data in each row
         data = count_mat.counts.iloc[i]
                 
-            #for the upper and lower outliers
-            Q1 = np.percentile(data, 25)
-            Q3 = np.percentile(data, 75)
-            IQR =  np.percentile(data, 75) - np.percentile(data, 25)
+        #for the upper and lower outliers
+        Q1 = np.percentile(data, 25)
+        Q3 = np.percentile(data, 75)
+        IQR =  np.percentile(data, 75) - np.percentile(data, 25)
                 
-            #for the histogram bin edges and count numbers
-            (n, bins, patches) = plt.hist(data, bins=20, label='hst')
+        #for the histogram bin edges and count numbers
+        (n, bins, patches) = plt.hist(data, bins=20, label='hst')
                 
-            #make the dict for each row
-            output['stats']['dists'].append({'name':CountMatrix(rowdist_pd_df).count_names[i], 'dist':list(n), 'bins':list(bins)[1:],'extrema':{'lower':[i for i in data if i < Q1-1.5*IQR], 'upper':[i for i in data if i > Q3+1.5*IQR]}})
+        #make the dict for each row
+        output['stats']['dists'].append({'name':CountMatrix(rowdist_pd_df).count_names[i], 'dist':list(n), 'bins':list(bins)[1:],'extrema':{'lower':[i for i in data if i < Q1-1.5*IQR], 'upper':[i for i in data if i > Q3+1.5*IQR]}})
         
-        return json.dumps(output, indent = 4, sort_keys = True)
+    return json.dumps(output, indent = 4, sort_keys = True)
 
 def colzero(count_mat) :
 	'''Column-wise distribution of zero counts'''

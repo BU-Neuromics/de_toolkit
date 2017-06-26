ba473cad3091b3e6f77c6f0c622d8c4f422c5897
Adam Labadorf <alabadorf@gmail.com>
2017-06-26 17:14:46 -0400

    added matplotlib to conda packages

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,3 +1,3 @@
 conda install -c conda-forge -c bioconda -c r python=3.5 docopt pandas pytest "rpy2>=2.7.3" bioconductor-deseq2 \
-  future "git>=1.8.2" git-lfs sphinx sphinx-autobuild patsy pyparsing
+  future "git>=1.8.2" git-lfs sphinx sphinx-autobuild patsy pyparsing matplotlib
 conda list > conda_packages.txt

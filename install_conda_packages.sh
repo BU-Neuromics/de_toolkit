bfcb582451c879979216d18c1b836e8eb4afe9e7
Adam Labadorf <alabadorf@gmail.com>
2017-07-05 16:09:27 -0400

    wordwrapped conda install script

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,3 +1,4 @@
-conda install -c conda-forge -c bioconda -c r python=3.5 docopt pandas pytest "rpy2>=2.7.3" bioconductor-deseq2 \
-  future sphinx sphinx-autobuild patsy pyparsing matplotlib
-conda list > conda_packages.txt
+conda install -c conda-forge -c bioconda -c r python=3.5 docopt pandas pytest \
+  "rpy2>=2.7.3" bioconductor-deseq2 future sphinx sphinx-autobuild patsy \
+  pyparsing matplotlib setuptools
+conda list --export > conda_packages.txt

5992f85eba584b8922ebfc7ec2506ef37e7b7882
Adam <alabadorf@gmail.com>
2017-09-24 20:16:47 +0000

    refactoring of r util functions

--- de_toolkit/util.py
+++ de_toolkit/util.py
@@ -1,16 +1,24 @@
 from .common import CountMatrixFile
 from contextlib import contextmanager
 
-def load_count_mat_file(count_fn) :
-  with open(count_fn) as f :
-    count_obj = CountMatrixFile(f)
-  return count_obj
-
-def require_rpy2(f) :
+################################################################################
+# all the rpy2 stuffs
+def check_rpy2() :
   try :
     import rpy2
+    return True
   except ImportError as e :
-    raise Exception('rpy2 must be installed to use this function')
+    return False
+
+class Rpy2ImportError(ImportError) : pass
+class RPackageMissingError(Exception) : pass
+
+def check_rpy2_or_raise() :
+  if not check_rpy2() :
+    raise Rpy2ImportError('rpy2 must be installed to use this function')
+
+def require_rpy2(f) :
+  check_rpy2_or_raise()
   return f
 
 class Stub(Exception): pass
@@ -20,17 +28,26 @@ def stub(f) :
   return stub
 
 @require_rpy2
-def require_deseq2(f) :
+def check_deseq2() :
   from rpy2.rinterface import RRuntimeError
   try :
     from rpy2.robjects.packages import importr
     deseq = importr('DESeq2')
+    return True
   except RRuntimeError as e :
-    raise Exception('DESeq2 must be installed to use this function')
+    return False
+
+def check_deseq2_or_raise():
+  if not check_deseq2() :
+    raise RPackageMissingError('DESeq2 must be installed to use this function')
+  return True
+
+def require_deseq2(f) :
+  check_deseq2_or_raise()
   return f
 
 @require_rpy2
-def count_obj_to_r_matrix(count_obj) :
+def pandas_dataframe_to_r_matrix(df) :
 
   from rpy2 import robjects
   import rpy2.rlike.container as rlc
@@ -39,55 +56,72 @@ def count_obj_to_r_matrix(count_obj) :
   base = importr('base')
 
   # create a numerical matrix of counts
-  nrow, ncol = count_obj.counts.shape
-  counts = base.matrix(
-    robjects.IntVector(count_obj.counts.unstack())
+  nrow, ncol = df.shape
+  rmat = base.matrix(
+    robjects.FloatVector(df.unstack())
     ,nrow=nrow
     ,ncol=ncol
   )
-  counts.rownames = robjects.StrVector(count_obj.counts.index.tolist())
-  counts.colnames = robjects.StrVector(count_obj.counts.columns.tolist())
+  rmat.rownames = robjects.StrVector(df.index.tolist())
+  rmat.colnames = robjects.StrVector(df.columns.tolist())
 
-  return counts
+  return rmat
 
+class RDatatypeException(Exception): pass
 @require_rpy2
-def column_data_rtype_dict(count_obj) :
+def pandas_dataframe_to_r_dataframe(df) :
+  '''return dictionary form of pandas dataframe *df* with values as vectors of
+  the appropriate R datatype (e.g. FloatVector)
+
+  Convert the pandas dataframe to an :py:`collections.OrderedDict` of R vectors
+  of the appropriate type according to the column Series dtype. Mapping is:
+
+  +-------------+---------------+
+  | numpy.dtype | robjects type |
+  +-------------+---------------+
+  |   float64   |  FloatVector  |
+  |    int64    |   IntVector   |
+  |    bool     |  BoolVector   |
+  |  O (object) |   StrVector   |
+  +-------------+---------------+
+
+  Unrecognized dtypes will raise an RDatatypeException.
+
+  :param df: pandas DataFrame object
+  :returns: :py:`collections.OrderedDict` of R vectors of the appropriate type
+  :rtype:  :py:`collections.OrderedDict`
+  '''
 
   from collections import OrderedDict
   import numpy
-  from rpy2 import robjects
-  import rpy2.rlike.container as rlc
+  from rpy2.robjects.vectors import DataFrame, BoolVector, FloatVector, IntVector, StrVector
   from rpy2.robjects.packages import importr
+  import rpy2.rlike.container as rlc
 
+  base = importr('base')
+
+  def error(x) :
+    dtype = x.dtype
+    raise RDatatypeException(('Unrecognized dtype in dataframe ({}), cannot convert '
+        'to r dataframe').format(dtype))
   dtype_to_rtype_map = {
-    numpy.dtype('float64'): robjects.FloatVector
-    ,numpy.dtype('int64'): robjects.IntVector
-    ,numpy.dtype('O'): robjects.StrVector
+    numpy.dtype('float64'): FloatVector
+    ,numpy.dtype('int64'): IntVector
+    ,numpy.dtype('bool'): BoolVector
+    # base.I prevents the StrVector from being converted to a FactorVector by R
+    ,numpy.dtype('O'): lambda x: base.I(StrVector(x))
   }
+
   d = OrderedDict()
 
-  for k,v in count_obj.column_data.iteritems() :
-    rtype = dtype_to_rtype_map[v.dtype]
-    d[k] = rtype(v.tolist())
+  for k,v in df.iteritems() :
+    rtype = dtype_to_rtype_map.get(v.dtype,error)
+    d[k] = rtype(v)
 
-  return d
+  r_df = DataFrame(d)
+  r_df.rownames = base.I(StrVector(df.index.tolist()))
 
-@require_rpy2
-def column_data_to_r_dataframe(count_obj) :
-
-  from rpy2 import robjects
-  import rpy2.rlike.container as rlc
-  from rpy2.robjects.packages import importr
-
-  # TODO: figure out how to pick the correct robject function
-  # based on pandas column dtype
-  col_data_kv = list(column_data_rtype_dict(count_obj).items())
-  od = rlc.OrdDict(col_data_kv)
-
-  colData = robjects.DataFrame(od)
-  colData.rownames = robjects.StrVector(count_obj.column_data.index.tolist())
-
-  return colData
+  return r_df
 
 @require_deseq2
 def count_obj_to_DESeq2(count_obj) :
@@ -97,16 +131,14 @@ def count_obj_to_DESeq2(count_obj) :
 
   base = importr('base')
 
-  # TODO: consider wrapping this in a try except
   deseq = importr('DESeq2')
 
-  countData = count_obj_to_r_matrix(count_obj)
+  countData = pandas_dataframe_to_r_matrix(count_obj.counts)
 
-  # create a dataframe of the column_data, if there are any
   if count_obj.column_data is None :
     raise Exception('DESeq2 requires colData, add column dataframe to count object')
 
-  colData = column_data_to_r_dataframe(count_obj)
+  colData = pandas_dataframe_to_r_dataframe(count_obj.column_data)
 
   # by default, the design is assumed the be in the first non-sample name column
   # of column_data
@@ -149,48 +181,4 @@ def rpy2_console_wrapper() :
   rpy2.rinterface.set_writeconsole_regular(old_writeconsole_regular)
   rpy2.rinterface.set_writeconsole_warnerror(old_writeconsole_warnerror)
 
-@require_rpy2
-def pandas_to_rmatrix(df) :
-
-  from rpy2 import robjects
-
-  cnts = df.as_matrix()
-
-  m = robjects.r.matrix(
-    robjects.FloatVector(
-      cnts.ravel()
-    )
-    ,nrow=cnts.shape[0]
-    ,byrow=True
-  )
-
-  return m
-
-@require_rpy2
-def pandas_to_rdataframe(df) :
-
-  from rpy2 import robjects
-  import rpy2.rlike.container as rlc
-
-  # this converts all columns in df to the appropriate
-  # R vector type (i.e. IntVector, FloatVector, StrVector, or BoolVector)
-
-  r_dict = []
-  for col, vals in df.items() :
-    f = None
-    dtype = vals.dtype
-    if dtype == int :
-      f = robjects.IntVector
-    elif dtype == float :
-      f = robjects.FloatVector
-    elif dtype == object :
-      f = robjects.StrVector
-    elif dtype == bool :
-      f = robjects.BoolVector
-    else :
-      raise Exception(('Unrecognized dtype in dataframe ({}), cannot convert '
-        'to r dataframe').format(dtype))
-
-    r_dict.append((str(col), f(vals)))
-
-  return robjects.DataFrame(rlc.OrdDict(r_dict))
+################################################################################

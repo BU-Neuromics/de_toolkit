9fe04ef26f28d7bf705bb526c0477ffb2115377e
Adam <alabadorf@gmail.com>
2017-09-25 23:23:42 +0000

    removed bioconda deseq2 package install

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,5 +1,4 @@
 conda install -c conda-forge -c bioconda python=3.5 docopt pandas pytest \
   future sphinx sphinx-autobuild patsy ipython statsmodels \
-  ply matplotlib setuptools scipy scikit-learn "rpy2>=2.7.3"
-conda install -c bioconda bioconductor-deseq2
+  ply matplotlib setuptools scipy scikit-learn "rpy2>=2.8.3"
 conda list --export > conda_packages.txt

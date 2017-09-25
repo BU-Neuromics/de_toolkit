947c4a57041ff36e33f8a5ee08654d20261349ea
Adam <alabadorf@gmail.com>
2017-09-25 16:01:16 +0000

    added statsmodels package requirement

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,5 +1,5 @@
 conda install -c conda-forge -c bioconda python=3.5 docopt pandas pytest \
-  future sphinx sphinx-autobuild patsy ipython \
+  future sphinx sphinx-autobuild patsy ipython statsmodels \
   ply matplotlib setuptools scipy scikit-learn "rpy2>=2.7.3"
 conda install -c bioconda bioconductor-deseq2
 conda list --export > conda_packages.txt

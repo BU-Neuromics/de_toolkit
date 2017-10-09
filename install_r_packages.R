fd446267c9580453553b3c7ee844bc97732937c2
Adam <alabadorf@gmail.com>
2017-10-09 14:10:05 +0000

    merged install r packages script, since we are installing bioconductor packages manually now

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,7 +1,17 @@
+# set the CRAN repo for the whole process so biocLite can also use it
+r <- getOption('repos')
+r['CRAN'] <- 'http://cran.rstudio.com/'
+options(repos = r)
+
+# standard R packages here
 packages <- c('logistf')
 lapply(
   packages
   ,function(package) {
-    install.packages(package, dependencies=TRUE, repos='http://cran.rstudio.com/')
+    install.packages(package, dependencies=TRUE)
   }
 )
+
+# bioconductor packages here
+source('http://bioconductor.org/biocLite.R')
+biocLite('DESeq2')

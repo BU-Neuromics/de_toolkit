07c22d08911298f51abbae92d319fd0a152dbf17
Kylie Shen <kylieshen@kylies-mbp-2.home>
2017-10-12 11:21:34 -0700

    Started to implement nonzero filter function and added tests for it

--- de_toolkit/filter.py
+++ de_toolkit/filter.py
@@ -1,13 +1,33 @@
-def filter_nonzero(count_mat,n=0.5,groups=None) :
-  '''
-  Filter rows from *count_mat* based on the number of zero counts.
+import numpy as np
+import pandas as pd
 
-  * if 0 < *n* < 1, then *n* is the fraction of samples that must be non-zero
-  * if 1 <= *n* < *count_mat.shape[1]*, then *n* is the number of samples that
-    must be non-zero
-  * if *groups* is not *None*, it must be a list of column indices or names of
-    samples that should be considered a group, and n is applied to each
-     group separately. Rows are filtered if all groups fail the criterion based
-     on *n*
-  '''
-  pass
+def filter_nonzero(count_mat,n=0.5,groups=None) :
+    '''
+      Filter rows from *count_mat* based on the number of zero counts.
+
+      * if 0 < *n* < 1, then *n* is the fraction of samples that must be non-zero
+      * if 1 <= *n* < *count_mat.shape[1]*, then *n* is the number of samples that
+        must be non-zero
+      * if *groups* is not *None*, it must be a list of column indices or names of
+        samples that should be considered a group, and n is applied to each
+        group separately. Rows are filtered if all groups fail the criterion based
+        on *n*
+     '''
+
+    cnts = count_mat.counts.as_matrix()
+    column_names = count_mat.sample_names
+    row_names = list(count_mat.count_names)
+    final_cnts = pd.DataFrame(columns=column_names)
+
+    if groups is None:
+      if 0 < n < 1:
+        for item, name in zip(cnts, row_names):
+          if np.count_nonzero(item)/len(item) > n:
+            final_cnts.loc[name] = list(item)
+    
+      elif 1 <= n <= len(cnts[0]):
+        for item, name in zip(cnts, row_names):
+          if np.count_nonzero(item) >= n:
+            final_cnts.loc[name] = list(item)
+
+    return final_cnts
---
+++ tests/filter/test_filter.py
@@ -0,0 +1,26 @@
+import docopt
+import pytest
+import pandas as pd
+from de_toolkit.filter import *
+
+def test_filter_nonzero_default(fake_counts_obj_with_zeros):
+	output = filter_nonzero(fake_counts_obj_with_zeros)
+	true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+	true_df.loc['gene1'] = [2.0, 4.0, 0.0]
+	true_df.loc['gene4'] = [5.0, 0.0, 125.0]
+	true_df.loc['gene5'] = [6.0, 36.0, 216.0]
+	assert true_df.equals(output)
+
+def test_filter_nonzero_fraction(fake_counts_obj_with_zeros):
+	output = filter_nonzero(fake_counts_obj_with_zeros, n=0.7)
+	true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+	true_df.loc['gene5'] = [6.0, 36.0, 216.0]
+	assert true_df.equals(output)
+
+def test_filter_nonzero_number(fake_counts_obj_with_zeros):
+	output = filter_nonzero(fake_counts_obj_with_zeros, n=2)
+	true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+	true_df.loc['gene1'] = [2.0, 4.0, 0.0]
+	true_df.loc['gene4'] = [5.0, 0.0, 125.0]
+	true_df.loc['gene5'] = [6.0, 36.0, 216.0]
+	assert true_df.equals(output)

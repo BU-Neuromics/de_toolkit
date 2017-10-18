52e25fab9a938c1e5c0187d3c51dcd7fbea4cddc
Kylie Shen <kylieshen@kylies-mbp-2.home>
2017-10-18 11:44:26 -0700

    Added tests for filter median function

--- tests/filter/test_filter.py
+++ tests/filter/test_filter.py
@@ -191,3 +191,39 @@ def test_filter_mean_e(fake_counts_obj_with_zeros):
     true_df = pd.DataFrame(columns=['a', 'b', 'c'])
     true_df.loc['gene2'] = [0.0, 9.0, 0.0]
     assert true_df.equals(output)
+
+def test_filter_median_lt(fake_counts_obj_with_zeros):
+    output = filter_median(fake_counts_obj_with_zeros, 2, '<')
+    true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    true_df.loc['gene2'] = [0.0, 9.0, 0.0]
+    true_df.loc['gene3'] = [4.0, 0.0, 0.0]
+    assert true_df.equals(output)
+
+def test_filter_median_lte(fake_counts_obj_with_zeros):
+    output = filter_median(fake_counts_obj_with_zeros, 2, '<=')
+    true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    true_df.loc['gene1'] = [2.0, 4.0, 0.0]
+    true_df.loc['gene2'] = [0.0, 9.0, 0.0]
+    true_df.loc['gene3'] = [4.0, 0.0, 0.0]
+    assert true_df.equals(output)
+
+def test_filter_median_gt(fake_counts_obj_with_zeros):
+    output = filter_median(fake_counts_obj_with_zeros, 2, '>')
+    true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    true_df.loc['gene4'] = [5.0, 0.0, 125.0]
+    true_df.loc['gene5'] = [6.0, 36.0, 216.0]
+    assert true_df.equals(output)
+
+def test_filter_median_gte(fake_counts_obj_with_zeros):
+    output = filter_median(fake_counts_obj_with_zeros, 2, '>=')
+    true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    true_df.loc['gene1'] = [2.0, 4.0, 0.0]
+    true_df.loc['gene4'] = [5.0, 0.0, 125.0]
+    true_df.loc['gene5'] = [6.0, 36.0, 216.0]
+    assert true_df.equals(output)
+
+def test_filter_median_e(fake_counts_obj_with_zeros):
+    output = filter_median(fake_counts_obj_with_zeros, 2, '=')
+    true_df = pd.DataFrame(columns=['a', 'b', 'c'])
+    true_df.loc['gene1'] = [2.0, 4.0, 0.0]
+    assert true_df.equals(output)

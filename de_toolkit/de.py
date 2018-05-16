d3ef605493bc14286e298896d346aec812b32545
Adam Labadorf <alabadorf@gmail.com>
2018-05-15 20:40:35 -0400

    wrapr update to firth and tests

--- de_toolkit/de.py
+++ de_toolkit/de.py
@@ -1,202 +1,168 @@
 '''
 Usage:
-  detk-de deseq2 [options] [--rda=RDA] <design> <count_fn> <cov_fn>
-  detk-de firth [options] [--rda=RDA] <design> <count_fn> <cov_fn>
-  detk-de t-test <count_fn> <cov_fn>
+    detk-de deseq2 [options] [--rda=RDA] <design> <count_fn> <cov_fn>
+    detk-de firth [options] [--rda=RDA] <design> <count_fn> <cov_fn>
+    detk-de t-test <count_fn> <cov_fn>
 
 Options:
-  -o FILE --output=FILE    Destination of primary output [default: stdout]
-  --rda=RDA                Filename passed to saveRDS() R function of the result
-                           objects from the analysis
-  --strict    Require that the sample order indicated by the column names in the
-              counts file are the same as, and in the same order as, the
-              sample order in the row names of the covariates file
+    -o FILE --output=FILE        Destination of primary output [default: stdout]
+    --rda=RDA                                Filename passed to saveRDS() R function of the result
+                                                     objects from the analysis
+    --strict        Require that the sample order indicated by the column names in the
+                            counts file are the same as, and in the same order as, the
+                            sample order in the row names of the covariates file
 
 '''
 from docopt import docopt
 import pandas
 import sys
 from .common import CountMatrixFile, InvalidDesignException
-from .util import (
-  pandas_dataframe_to_r_dataframe
-  ,pandas_dataframe_to_r_matrix
-  ,require_rpy2
-  ,require_deseq2
-  ,stub
-)
+from .wrapr import (
+        require_r, require_deseq2, wrapr, RExecutionError, RPackageMissing,
+        require_r_package
+    )
+from .util import stub
 
 @require_deseq2
 def deseq2(count_obj) :
-  import rpy2
-  pass
+    pass
 
-@require_rpy2
-def firth_logistic_regression(count_obj,rda=None) :
+@require_r('logistf')
+def firth_logistic_regression(
+        count_obj,
+        standardize=False,
+        rda=None,
+        cores=None) :
 
-  # make a copy of count_obj, since we mutate it
-  count_obj = count_obj.copy()
+    # make a copy of count_obj, since we mutate it
+    count_obj = count_obj.copy()
 
-  # validate the design matrix
-  if count_obj.design is None or count_obj.design_matrix is None :
-    raise InvalidDesignException('count_obj must have a design matrix in Firth'
-      ' logistic regression')
+    # validate the design matrix
+    if count_obj.design is None or count_obj.design_matrix is None :
+        raise InvalidDesignException('count_obj must have a design matrix in Firth'
+            ' logistic regression')
 
-  if 'counts' not in count_obj.design_matrix.rhs :
-    raise InvalidDesignException('The term "counts" must exist on the right hand'
-      'side of the model in Firth logistic regression')
+    if 'counts' not in count_obj.design_matrix.rhs :
+        raise InvalidDesignException('The term "counts" must exist on the right hand'
+            'side of the model in Firth logistic regression')
 
-  # make sure the rhs of the design matrix doesn't have an intercept
-  count_obj.design_matrix.drop_from_rhs('Intercept',quiet=True)
+    # make sure the rhs of the design matrix doesn't have an intercept
+    count_obj.design_matrix.drop_from_rhs('Intercept',quiet=True)
 
-  from rpy2 import robjects
-  import rpy2.rlike.container as rlc
-  from rpy2.robjects.packages import importr
-  from rpy2.rinterface import RRuntimeError
+    if cores is not None :
+        require_r_package('parallel')
+        try :
+            cores = int(cores)
+        except ValueError :
+            raise Exception('The cores argument to firth_logistic_regression '
+                    'must be an integer')
 
-  base = importr('base')
+    params = {
+        'design': count_obj.design,
+        'standardize': standardize,
+        'rda': rda,
+        'cores': cores
+    }
+    script = '''\
+        library(logistf)
+        cnts <- read.csv(counts.fn,header=T,as.is=T)
+        index.name <- names(cnts)[1]
+        rownames(cnts) <- cnts[[1]]
+        cnts <- cnts[c(-1)]
 
-  try :
-    logistf = importr('logistf')
-  except RRuntimeError as e :
-    raise Exception('logistf must be installed to use this function')
+        rnames <- rownames(cnts)
+        cnts <- data.frame(lapply(cnts,as.numeric))
+        rownames(cnts) <- rnames
 
-  design_formula = robjects.Formula(count_obj.design)
+        # scale counts to obtain standardized beta estimates
+        if(params$standardize) {
+            cnts <- data.frame(t(scale(t(cnts))))
+        }
 
-  # fits stores a list of tuples of the form (rfit,'status')
-  # the status records info from firth about the specific gene
-  # currently it only stores whether the profile likelihood or Wald
-  # method was used to calculate confidence intervals
-  fits = []
-  for i in range(count_obj.counts.shape[0]) :
-    gene_counts = count_obj.counts.iloc[i]
-    #colData_rtype_dict['counts'] = robjects.FloatVector(gene_counts)
+        # design formula
+        form <- params$design
 
-    count_obj.design_matrix.update_design('counts',gene_counts)
+        # load design matrix
+        design.mat <- read.csv(metadata.fn,header=T,as.is=T,row.names=1)
 
-    # the R object we will pass to 
-    full_matrix_robj = pandas_dataframe_to_r_dataframe(
-      count_obj.design_matrix.full_matrix
-    )
+        fit <- NULL
 
-    # issue #3 is caused by the Firth profile likelihood based confidence
-    # interval method producing incoherent results on certain data (can't
-    # figure out what exactly)
-    # attempt the regression with profile likelihood on by default and if
-    # it crashes fall back on the Wald method and move on
-    try :
-      fit = (
-        logistf.logistf(design_formula,data=full_matrix_robj,pl=True)
-        ,'PL'
-      )
-    except RRuntimeError as exc :
-      # capture this exact error, otherwise raise
-      if 'NA/NaN/Inf in foreign function call (arg 10)' in str(exc) :
-        fit = (
-          logistf.logistf(design_formula,data=full_matrix_robj,pl=False)
-          ,'Wald'
+        applyf <- lapply
+        if(!is.null(params$cores)) {
+            library(parallel)
+            applyf <- function(l,f) { mclapply(l,f,cores=params$cores) }
+        }
+        res.orig <- applyf(rownames(cnts),
+            function(gene) {
+              x <- data.frame(design.mat)
+              x$counts <- unlist(cnts[gene,])
+              log.fit <- logistf(formula(form),data=x,pl=F)
+              fit <<- log.fit
+              out <- c(gene, 'OK')
+              names(out) <- c(index.name,'status')
+              coeffs <- log.fit$coeff
+              names(coeffs) <- paste0(names(coeffs),'__beta')
+              probs <- log.fit$prob
+              names(probs) <- paste0(names(probs),'__p')
+              padj <- rep(NA,length(probs))
+              names(padj) <- paste0(names(log.fit$prob),'__padj')
+              cp <- c(rbind(coeffs,probs,padj))
+              cp.names <- c(rbind(names(coeffs),names(probs),names(padj)))
+              cp.names <- gsub(".Intercept.","int",cp.names)
+              names(cp) <- cp.names
+              c(out,cp)
+            }
         )
-      else :
-        raise
 
-    fits.append(fit)
+        if(!is.null(params$rda)) {
+            saveRDS(fit,params$rda)
+        }
+        res <- do.call(rbind,res.orig)
+        res.df <- as.data.frame(res,stringsAsFactors=F)
+        for(c in colnames(res.df)[c(-1,-2)]) {
+            res.df[c] <- as.numeric(res.df[c][[1]])
+        }
+        # calculate p.adjust for each pvalue col
+        for(c in Filter(function(x) endsWith(x,'__p'),colnames(res.df))) {
+            res.df[paste0(c,'adj')] <- p.adjust(res.df[[c]],"fdr")
+        }
 
-  # members of fit list
-  # "coefficients"      "alpha"      "terms"      "var"
-  # "formula"           "call"       "conv"       "firth"
-  # "method.ci"         "ci.lower"   "ci.upper"   "conflev"
-  # "df"                "loglik"     "iter"       "n"       "y"
-  # "linear.predictors" "predict"    "hat.diag"   "method"  "prob"
-  # "pl.iter"           "betahist"   "pl.conv"    "data"    "weights"
+        write.csv(res.df,counts.out.fn,row.names=F)
+    '''
 
-  fields = ('beta','p','padj'
-    #,'ci.upper','ci.lower','loglik'
-  )
-
-  # these are the model variables
-  var_names = list(fits[0][0].rx2('terms'))
-
-  int_i = var_names.index('(Intercept)')
-  var_names[int_i] = 'int'
-  var_names = [_.replace(' ','_') for _ in var_names]
-
-  colnames = ['{}__{}'.format(_i,_j) for _i in var_names for _j in fields]
-
-  firth_props = pandas.DataFrame(
-   index=count_obj.counts.index.tolist()
-   ,columns=colnames+['status']
-  )
-
-  for i,row in enumerate(firth_props.index) :
-
-    fit,status = fits[i]
-
-    # coefficients
-    cols = ['{}__beta'.format(_) for _ in var_names]
-    firth_props.loc[row,cols] = fit.rx2('coefficients')
-
-    # p-values
-    cols = ['{}__p'.format(_) for _ in var_names]
-    firth_props.loc[row,cols] = fit.rx2('prob')
-
-    # ci.upper
-    #cols = ['{}__ci.upper'.format(_) for _ in var_names]
-    #firth_props.loc[row,cols] = fit.rx2('ci.upper')
-
-    # ci.lower
-    #cols = ['{}__ci.lower'.format(_) for _ in var_names]
-    #firth_props.loc[row,cols] = fit.rx2('ci.lower')
-
-    # loglik
-    #cols = ['{}__loglik'.format(_) for _ in var_names]
-    #firth_props.loc[row,cols] = fit.rx2('loglik')
-
-    # status
-    firth_props.loc[row,'status'] = status
-
-  stats = importr('stats')
-
-  for var in var_names :
-    p_adjust = stats.p_adjust(
-      robjects.FloatVector(firth_props['{}__p'.format(var)])
-      ,method = 'BH'
-    )
-    firth_props['{}__padj'.format(var)] = p_adjust
-
-  if rda is not None :
-    robjects.r['saveRDS'](fits,rda)
-
-  # cast all columns to numeric if possible
-  firth_props = firth_props.apply(pandas.to_numeric,errors='ignore')
-
-  return firth_props
+    with wrapr(script,
+            counts=count_obj.counts,
+            metadata=count_obj.design_matrix.full_matrix,
+            params=params) as wr :
+        return wr.counts_out
 
 @stub
 def t_test(count_obj) :
-  pass
+    pass
 
 def main(argv=None) :
 
-  args = docopt(__doc__,argv=argv)
+    args = docopt(__doc__,argv=argv)
 
-  count_obj = CountMatrixFile(
-    args['<count_fn>']
-    ,args['<cov_fn>']
-    ,design=args['<design>']
-    ,strict=args.get('--strict',False)
-  )
+    count_obj = CountMatrixFile(
+        args['<count_fn>']
+        ,args['<cov_fn>']
+        ,design=args['<design>']
+        ,strict=args.get('--strict',False)
+    )
 
-  if args['deseq2'] :
-    deseq2(count_obj)
-  elif args['firth'] :
-    firth_out = firth_logistic_regression(count_obj,rda=args['--rda'])
+    if args['deseq2'] :
+        deseq2(count_obj)
+    elif args['firth'] :
+        firth_out = firth_logistic_regression(count_obj,rda=args['--rda'])
 
-    if args['--output'] == 'stdout' :
-      f = sys.stdout
-    else :
-      f = args['--output']
+        if args['--output'] == 'stdout' :
+            f = sys.stdout
+        else :
+            f = args['--output']
 
-    firth_out.to_csv(f,sep='\t')
+        firth_out.to_csv(f,sep='\t')
 
 if __name__ == '__main__' :
 
-  main()
+    main()

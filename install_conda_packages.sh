c27e64f3bf1dcce2c5babc48934ae55c2072734f
Adam Labadorf <labadorf@bu.edu>
2018-07-09 18:20:24 -0400

    added R install packages

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,3 +1,6 @@
-conda install -c bioconda python=3.6
+# r-nloptr is needed by logistf but fails on install from CRAN
+# install here
+conda install -c r -c bioconda python=3.6 r-base r-nloptr
 pip install -r requirements.txt
 conda list --export > conda_packages.txt
+Rscript install_r_packages.R

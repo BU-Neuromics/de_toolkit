cd0b91ce4a9670bc63f977353ce4ccd03e2cd2c2
Adam Labadorf <labadorf@bu.edu>
2018-07-10 18:22:19 -0400

    4 space indent

--- de_toolkit/transform.py
+++ de_toolkit/transform.py
@@ -1,102 +1,102 @@
 '''
 Usage:
-  detk-transform vst <counts_fn> <cov_fn>
-  detk-transform ruvseq <counts_fn>
-  detk-transform trim <counts_fn>
-  detk-transform shrink <counts_fn>
+    detk-transform vst <counts_fn> <cov_fn>
+    detk-transform ruvseq <counts_fn>
+    detk-transform trim <counts_fn>
+    detk-transform shrink <counts_fn>
 '''
 from docopt import docopt
 from .common import CountMatrixFile
 from .wrapr import (
-        require_r, require_deseq2, wrapr, RExecutionError, RPackageMissing,
-        require_r_package
-    )
+                require_r, require_deseq2, wrapr, RExecutionError, RPackageMissing,
+                require_r_package
+        )
 from .util import stub
 
 def pmf_transform(x,shrink_factor=0.25,max_p=None,iters=1000) :
 
-  x = x.copy()
-  max_p = max_p or sqrt(1./len(x))
+    x = x.copy()
+    max_p = max_p or sqrt(1./len(x))
 
-  for i in range(iters) :
-    p_x = x/x.sum()
+    for i in range(iters) :
+        p_x = x/x.sum()
 
-    if x.sum() == 0 :
-      print('all samples set to zero, returning')
-      break
+        if x.sum() == 0 :
+            print('all samples set to zero, returning')
+            break
 
-    p_x_outliers = p_x>max_p
+        p_x_outliers = p_x>max_p
 
-    if not any(p_x_outliers) :
-      break # done
+        if not any(p_x_outliers) :
+            break # done
 
-    max_non_outliers = max(x[~p_x_outliers])
+        max_non_outliers = max(x[~p_x_outliers])
 
-    x[p_x_outliers] = max_non_outliers+(x[p_x_outliers]-max_non_outliers)*shrink_factor
+        x[p_x_outliers] = max_non_outliers+(x[p_x_outliers]-max_non_outliers)*shrink_factor
 
-  if i == iters :
-    print('PMF transform did not converge')
-    print(p_x)
-    print(p_x_outliers)
+    if i == iters :
+        print('PMF transform did not converge')
+        print(p_x)
+        print(p_x_outliers)
 
-  return x
+    return x
 
 @stub
 def shrink_outliers(count_obj) :
-  pass
+    pass
 
 @stub
 def trim_outliers(count_obj) :
-  pass
+    pass
 
 @require_deseq2
 def vst(count_obj) :
-  import pandas
-  from rpy2 import robjects
-  from rpy2.rinterface import RRuntimeError
-  import rpy2.rlike.container as rlc
-  from rpy2.robjects.packages import importr
-  import warnings
+    import pandas
+    from rpy2 import robjects
+    from rpy2.rinterface import RRuntimeError
+    import rpy2.rlike.container as rlc
+    from rpy2.robjects.packages import importr
+    import warnings
 
-  base = importr('base')
+    base = importr('base')
 
-  deseq = importr('DESeq2')
-  sumexp = importr('SummarizedExperiment')
+    deseq = importr('DESeq2')
+    sumexp = importr('SummarizedExperiment')
 
-  dds = count_obj_to_DESeq2(count_obj)
+    dds = count_obj_to_DESeq2(count_obj)
 
-  with rpy2_console_wrapper() as rpy2_f :
-    try :
-      vsd = deseq.varianceStabilizingTransformation(dds)
-      vsd_values = sumexp.assay(vsd)
-    except RRuntimeError as e :
-      print(e)
-      raise
+    with rpy2_console_wrapper() as rpy2_f :
+        try :
+            vsd = deseq.varianceStabilizingTransformation(dds)
+            vsd_values = sumexp.assay(vsd)
+        except RRuntimeError as e :
+            print(e)
+            raise
 
-  return vsd_values
+    return vsd_values
 
 @stub
 def ruvseq(count_obj) :
-  pass
+    pass
 
 def main(argv=None) :
 
-  args = docopt(__doc__,argv=argv)
+    args = docopt(__doc__,argv=argv)
 
-  count_obj = CountMatrixFile(
-    args['<counts_fn>']
-    ,column_data_f=args['<cov_fn>']
-  )
+    count_obj = CountMatrixFile(
+        args['<counts_fn>']
+        ,column_data_f=args['<cov_fn>']
+    )
 
-  if args['vst'] :
-    vst_counts = vst(count_obj)
+    if args['vst'] :
+        vst_counts = vst(count_obj)
 
-  elif args['ruvseq'] :
-    ruvseq(count_obj)
-  elif args['trim'] :
-    trim_outliers(count_obj)
-  elif args['shrink'] :
-    shrink_outliers(count_obj)
+    elif args['ruvseq'] :
+        ruvseq(count_obj)
+    elif args['trim'] :
+        trim_outliers(count_obj)
+    elif args['shrink'] :
+        shrink_outliers(count_obj)
 
 if __name__ == '__main__' :
-  main()
+    main()
--- tests/transform/test_transform.py
+++ tests/transform/test_transform.py
@@ -4,40 +4,40 @@ from de_toolkit.transform import main
 import warnings
 
 def test_transform_cli(fake_counts_csv) :
-  with pytest.raises(docopt.DocoptExit) :
-    main(argv=None)
+    with pytest.raises(docopt.DocoptExit) :
+        main(argv=None)
 
 def test_transform_vst_cli(fake_big_counts_csv,fake_column_data_csv):
 
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    main(['vst',fake_big_counts_csv,fake_column_data_csv])
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        main(['vst',fake_big_counts_csv,fake_column_data_csv])
 
 def test_transform_vst(fake_big_counts_obj):
-  from de_toolkit.transform import vst
+    from de_toolkit.transform import vst
 
-  fake_big_counts_obj.design = 'counts ~ category[cont]'
+    fake_big_counts_obj.design = 'counts ~ category[cont]'
 
-  with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    vst(fake_big_counts_obj)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        vst(fake_big_counts_obj)
 
 """
 def test_transform_ruvseq_cli(fake_counts_csv):
-  main(['ruvseq',fake_counts_csv])
+    main(['ruvseq',fake_counts_csv])
 def test_transform_ruvseq(fake_counts_csv):
-  from de_toolkit.transform import ruvseq
-  pass
+    from de_toolkit.transform import ruvseq
+    pass
 
 def test_transform_trim_cli(fake_counts_csv):
-  main(['trim',fake_counts_csv])
+    main(['trim',fake_counts_csv])
 def test_transform_trim(fake_counts_csv):
-  from de_toolkit.transform import trim_outliers
-  pass
+    from de_toolkit.transform import trim_outliers
+    pass
 
 def test_transform_shrink_cli(fake_counts_csv):
-  main(['shrink',fake_counts_csv])
+    main(['shrink',fake_counts_csv])
 def test_transform_shrink(fake_counts) :
-  from de_toolkit.transform import shrink_outliers
-  pass
+    from de_toolkit.transform import shrink_outliers
+    pass
 """

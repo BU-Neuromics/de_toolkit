2569e1af9d2b26e63cd827121112378cef3b5a4b
Adam Labadorf <labadorf@bu.edu>
2018-11-27 17:01:43 -0500

    colzero parallel coordinate plot

--- de_toolkit/templates/js/colzero.js
+++ de_toolkit/templates/js/colzero.js
@@ -1,20 +1,76 @@
-                var container = $("#"+elem+" > #colzero_count")[0];
+                var container = $("#"+elem+" > #colzero_count")[0],
+                    bounds = {
+                    mean_max: 0, mean_min: Number.MAX_SAFE_INTEGER,
+                    median_max: 0, median_min: Number.MAX_SAFE_INTEGER
+                };
+                d.properties.zeros.map(
+                    function(x) {
+                        bounds.mean_max = Math.max(x.mean, x.nonzero_mean, bounds.mean_max);
+                        bounds.mean_min = Math.min(x.mean, x.nonzero_mean, bounds.mean_min);
+                        bounds.median_max = Math.max(x.median, x.nonzero_median, bounds.median_max);
+                        bounds.median_min = Math.min(x.median, x.nonzero_median, bounds.median_min);
+                    }
+                )
                 Highcharts.chart(container.id, {
-                    chart: { type: 'column' },
-                    title: { text: 'colzero' },
-                    xAxis: { title: { text: 'Sample' }, },
-                    yAxis: { title: { text: '# zeros' } },
-                    series: [{
-                        name: 'Zero counts',
-                        data: _.map(
-                            _.sortBy(d.properties.zeros,'zero_count'),
-                                function(x) {
-                                    return {
-                                        name: x.name,
-                                        color: '#00FF00',
-                                        y: x.zero_count
+                    chart: { parallelCoordinates: true },
+                    title: { text: 'Column Zeros' },
+                    xAxis: {
+                        categories: ['# Zero','Zero frac','Mean','Nonzero Mean', 'Median','Non-zero median'],
+                        labels: {
+                            styles: {
+                                color: '#DFDFDF'
+                            }
+                        }
+                    },
+                    yAxis: [
+                        {}, // # zero
+                        {}, // # zero frac
+                        {
+                            max: bounds.mean_max,
+                            min: bounds.mean_min
+                        }, // mean
+                        {
+                            max: bounds.mean_max,
+                            min: bounds.mean_min
+                        }, // nonzero mean
+                        {
+                            max: bounds.median_max,
+                            min: bounds.median_min
+                        }, // median
+                        {
+                            max: bounds.median_max,
+                            min: bounds.median_min
+                        }, // nonzero median
+                    ],
+                    plotOptions: {
+                        series: {
+                            animation: false,
+                            states: {
+                                hover: {
+                                    halo: {
+                                        size: 0
                                     }
-                            })
-                    }]
+                                }
+                            },
+                            events: {
+                                mouseOver: function() {
+                                    this.group.toFront();
+                                }
+                            }
+                        }
+                    },
+                    colors: ['rgba(11, 200, 200, 0.3)'],
+                    series: d.properties.zeros.map(
+                        function(x,i) {
+                            return {
+                                name: x.name,
+                                data: [
+                                    x.zero_count, x.zero_frac,
+                                    x.mean, x.nonzero_mean,
+                                    x.median, x.nonzero_median
+                                ]
+                            }
+                        }
+                    )
                 });
 

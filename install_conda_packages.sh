fb1316a9930d0a959290ffd901dc716121ace560
Adam Labadorf <alabadorf@gmail.com>
2019-03-09 17:04:03 -0500

    removed conda and pip files in favor of pipenv

--- install_conda_packages.sh
+++
@@ -1,8 +0,0 @@
-# r-nloptr is needed by logistf but fails on install from CRAN
-# install here
-conda install -c r -c bioconda -c conda-forge python=3.6 r-base r-nloptr \
-    bioconductor-deseq2 bioconductor-genomeinfodbdata r-logistf bioconductor-fgsea
-# GenomeInfoDb appears to be broken in conda
-R -e 'source("http://bioconductor.org/biocLite.R"); biocLite("GenomeInfoDb")'
-pip install -r requirements.txt
-conda list --export > conda_packages.txt

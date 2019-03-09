157915166f108cb8a411194bd62f32295c5743f3
Adam Labadorf <alabadorf@gmail.com>
2019-03-09 17:45:08 -0500

    Revert "fixed incorrect error type in design matrix column drop"
    This reverts commit 0fab9bbe5bd6528096893ad00c6c0054b5111b5b.

--- de_toolkit/patsy_lite.py
+++ de_toolkit/patsy_lite.py
@@ -197,14 +197,14 @@ class DesignMatrix(object) :
     def drop_from_lhs(self,column,quiet=False) :
         try :
             self.lhs.drop(column,axis=1,inplace=True)
-        except ValueError as e :
+        except KeyError as e :
             if not quiet :
                 raise ModelError('Cannot drop {} from lhs, does not exist'.format(column))
 
     def drop_from_rhs(self,column,quiet=False) :
         try :
             self.rhs.drop(column,axis=1,inplace=True)
-        except ValueError as e :
+        except KeyError as e :
             if not quiet :
                 raise ModelError('Cannot drop {} from rhs, does not exist'.format(column))
 
